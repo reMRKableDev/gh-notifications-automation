2b5a868673a003c5aa4bee8da3cea5f8f1b0f83d
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-01-16 00:43:53 +0100

    :art: Another test

--- src/config.js
+++ src/config.js
@@ -5,6 +5,8 @@ console.log("🚀 ~ process.env.NODE_ENV:", process.env.NODE_ENV);
 console.log("🚀 ~ process.env.GH_TOKEN:", process.env.GH_TOKEN);
 console.log("🚀 ~ process.env.GH_USERNAME:", process.env.GH_USERNAME);
 
+console.log("🚀 ~ secrets.GH_TOKEN:", secrets.GH_TOKEN);
+console.log("🚀 ~ secrets.GH_USERNAME:", secrets.GH_USERNAME);
 module.exports = {
   GITHUB_API: "https://api.github.com",
   GITHUB_TOKEN: process.env.GH_TOKEN,
