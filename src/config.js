d9e9db59ad85568939e4303ff249adfc3297cd7d
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-01-17 21:44:00 +0100

    :bug: Bad bug

--- src/config.js
+++ src/config.js
@@ -6,11 +6,6 @@ const config = {
   GITHUB_API: "https://api.github.com",
   GITHUB_TOKEN: process.env.GH_TOKEN,
   GITHUB_USERNAME: process.env.GH_USERNAME,
-  HEADERS: {
-    Authorization: `Bearer ${process.env.GH_TOKEN}`,
-    Accept: "application/vnd.github.v3+json",
-    "User-Agent": "GH-Notification-Automation",
-  },
 };
 console.log("🚀 ~ config:", config)
 
