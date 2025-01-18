a8375f1fb152b3984696b2906a965d8f31edd629
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-01-18 18:49:57 +0100

    :mute: Delete logs that are not sued

--- src/config.js
+++ src/config.js
@@ -2,13 +2,6 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const config = {
-  GITHUB_API: "https://api.github.com",
-  GITHUB_TOKEN: process.env.GH_TOKEN,
-  GITHUB_USERNAME: process.env.GH_USERNAME,
-};
-console.log("🚀 ~ config:", config)
-
 module.exports = {
   GITHUB_API: "https://api.github.com",
   GITHUB_TOKEN: process.env.GH_TOKEN,
